4947172c664ad5a6bc7ff1e5fd12500a33f74c5f
jorge juarez <j.juarez@comm.smt.org>
2024-09-16 10:21:32 -0300

    Se crea carousel con diseno responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import NavbarC from "./components/NavbarC";
+import CarouselC from "./components/CarouselC";
 import "./App.css";
 
 const App = () => {
-  return <NavbarC />;
+  return (
+    <>
+      <NavbarC />
+      <main>
+        <CarouselC />
+      </main>
+    </>
+  );
 };
 
 export default App;
