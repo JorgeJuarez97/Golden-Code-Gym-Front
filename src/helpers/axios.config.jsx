410ec70aaa0640eefa10f19ae75d82dc2d8cd69e
Jorge Daniel Juarez <jorgejuarez0407@gmail.com>
2025-01-05 21:32:56 -0300

    se agrego variable de back deploy

--- src/helpers/axios.config.jsx
+++ src/helpers/axios.config.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const clientAxios = axios.create({
-  baseURL: `${import.meta.env.VITE_URL_BACK_DEPLOY}`,
+  baseURL: `${import.meta.env.VITE_URL_BACK_LOCAL}`,
 });
 
 export default clientAxios;
